c30c3f41500ccb6b91b6cf20b7e5ec830ceca32c
VigneshG04 <vgur0004@student.monash.edu>
2024-10-11 12:33:13 +1100

    :frowning:

--- charts.js
+++ charts.js
@@ -7,3 +7,8 @@ var vg_2 = "hdi_gni.json";
 vegaEmbed("#hdi_gni", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "hdi_inequality.json";
+vegaEmbed("#hdi_inequality", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
